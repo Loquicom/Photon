cec140ba79852a5ed24c3ffe8ac17904aa8c7df3
Loquicom <Loquicom@users.noreply.github.com>
2019-11-25 17:22:19 +0100

    Création fichier check

---
+++ src/check.js
@@ -0,0 +1,50 @@
+const {exec} = require('child_process');
+const pf = require('portfinder');
+const file = require('./lib/file');
+
+async function getPhpVer(path = '') {
+    return new Promise(resolve => {
+        exec(path + 'php -v', (err, stdout) => {
+            if (err) {
+                resolve(undefined);
+                return;
+            }
+            const version = stdout.split('(')[0].replace('PHP', '').trim();
+            resolve(version);
+        });
+    });
+}
+
+module.exports.php = async function (localpath = './') {
+    let ver;
+    if (file.exist(localpath + 'bin/php/php')) {
+        ver = await getPhpVer(localpath + 'bin/php/php');
+        if (ver) {
+            return {
+                'version': ver,
+                'local': true
+            };
+        }
+    }
+    ver = await getPhpVer();
+    if (ver) {
+        return {
+            'version': ver,
+            'local': false
+        };
+    }
+    return undefined;
+};
+
+module.exports.port = async function (num = 1, start = undefined) {
+    start = start || Math.floor(Math.random() * 2000) + 49152;
+    let result = [];
+    for (let i = 0; i < num; i++) {
+        if (i > 0) {
+            start = result[i - 1] + 1;
+        }
+        pf.basePort = start;
+        result.push(await pf.getPortPromise());
+    }
+    return result;
+};
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-const { app, BrowserView, BrowserWindow } = require('electron');
-const { spawn } = require('child_process');
-const pf = require('portfinder');
+const {app, BrowserView, BrowserWindow} = require('electron');
+const {spawn} = require('child_process');
+const check = require('./check');
 const file = require('./lib/file');
 const config = require('../config');
 
 // Action when install/uninstall on Windows
 if (require('electron-squirrel-startup')) {
-  app.quit();
+    app.quit();
 }
 
 // Create tmp dir
@@ -24,58 +24,57 @@ let phpPort;
 let nodePort;
 
 async function main() {
-  // Find port for the servers
-  pf.basePort = Math.floor(Math.random() * 2000) + 49152
-  phpPort = await pf.getPortPromise();
-  pf.basePort = phpPort++;
-  nodePort = await pf.getPortPromise();
-  // Launch PHP server
-  phpServ = spawn('php', ['-S', `localhost:${phpPort}`, '-t', `${__dirname}/../app/`, `${__dirname}/php/router.php`]);
-  // Create window
-  createWindow();
-};
+    // Check if PHP exist and get the version
+    const php = await check.php();
+    // Find port for the servers
+    [phpPort, nodePort] = await check.port(2);
+    // Launch PHP server
+    phpServ = spawn('php', ['-S', `localhost:${phpPort}`, '-t', `${__dirname}/../app/`, `${__dirname}/php/router.php`]);
+    // Create window
+    createWindow();
+}
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: config.window.width,
-    height: config.window.height,
-    webPreferences: {
-      nodeIntegration: true
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: config.window.width,
+        height: config.window.height,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    // and load the index.html of the app.
+    mainWindow.loadFile(`${__dirname}/index.html`);
+    // Open the DevTools.
+    console.log(config.devtool, phpPort);
+    if (config.devtool) {
+        mainWindow.webContents.openDevTools();
     }
-  });
-  // and load the index.html of the app.
-  mainWindow.loadFile(`${__dirname}/index.html`);
-  // Open the DevTools.
-  console.log(config.devtool, phpPort);
-  if(config.devtool) {
-    mainWindow.webContents.openDevTools();
-  }
-  // Call the php server
-  let view = new BrowserView();
-  mainWindow.setBrowserView(view);
-  view.setBounds({ x: 0, y: 0, width: 800, height: 600 });
-  view.webContents.loadURL(`http://localhost:${phpPort}`);
-  view.setAutoResize({
-    width: true,
-    height: true
-  });
+    // Call the php server
+    let view = new BrowserView();
+    //mainWindow.setBrowserView(view);
+    view.setBounds({x: 0, y: 0, width: 800, height: 600});
+    view.webContents.loadURL(`http://localhost:${phpPort}`);
+    view.setAutoResize({
+        width: true,
+        height: true
+    });
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-};
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
 
 app.on('ready', main);
 app.on('window-all-closed', () => {
-  spawn('kill', ['-9', phpServ.pid]);
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    spawn('kill', ['-9', phpServ.pid]);
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 app.on('activate', () => {
-  if (mainWindow === null) {
-    phpServ = spawn('php', ['-S', `localhost:${phpPort}`, '-t', 'app/', `${__dirname}/php/router.php`]);
-    createWindow();
-  }
+    if (mainWindow === null) {
+        phpServ = spawn('php', ['-S', `localhost:${phpPort}`, '-t', 'app/', `${__dirname}/php/router.php`]);
+        createWindow();
+    }
 });
